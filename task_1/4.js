4fdbc9426c2c99599f186d2f69fb54f781c7636f
nayavii <nayavii17@gmail.com>
2022-11-20 19:57:11 +0100

    added task-1

---
+++ task_1/4.js
@@ -0,0 +1,7 @@
+// Напишите функцию isEven(n), которая принимает оди параметр, число, и возвращает, четное ли оно.
+
+const isEven = function (number) {
+  return number % 2 === 0;
+}
+
+console.log (isEven(4))
